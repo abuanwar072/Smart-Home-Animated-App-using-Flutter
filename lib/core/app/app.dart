d7c39480344688c6d83b7269b33ab563f06e5d1d
Abu Anwar MD Abdullah <abuanwar072@gmail.com>
2023-10-01 08:11:55 -0500

    pak & SmartHomeApp created

---
+++ lib/core/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SmartHomeApp extends StatelessWidget {
+  const SmartHomeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Container(),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:smart_home_animation/main.dart';
+import 'package:smart_home_animation/core/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SmartHomeApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
